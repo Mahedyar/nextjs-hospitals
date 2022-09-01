1405dc1f1808d016f45661563c0427ce70a3d551
Mahed <mahedyar@gmail.com>
2022-09-01 20:40:45 +0430

    trying to build 7

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,12 @@
 import Head from "next/head";
 import Hospitals from "../components/Hospitals/Hospitals";
 import Navigation from "../components/Navigation/Navigation";
+import HospitalJsonToObject from "../lib/HospitalJsonToObject";
 import HOSPITALS from "../lib/HOSPITALS";
-// import fsPromises from "fs/promises"
-// import fs from "fs";
-// import path from "path";
 // import { getStaticData } from "../lib/DataManager";
 
-// export async function getStaticProps() {
-//   const filePath = path.join(process.cwd(),'\\lib\\HospitalsJSON.json')
-//   const jsonData = await fsPromises.readFile(filePath)
-//   const hospitals = JSON.parse(jsonData)
-//   // console.log(hospitals)
-//   return {
-//     props:hospitals
-//   }
-// }
-// export async function getStaticProps() {
-//   const hospitalsData = getStaticData();
-//   return {
-//     props: {
-//       hospitalsData,
-//     },
-//   };
-// }
-
-// export  function getStaticData() {
-//   const filePath = path.join(process.cwd(), "\\lib\\HospitalsJSON.json");
-//   // const jsonData = await fsPromises.readFile(filePath);
-//   const jsonData = fs.readdirSync(filePath)
-//   const objectData = JSON.parse(jsonData);
-//   // console.log(objectData)
-//   return {
-//     props: objectData,
-//   };
-// }
-import { getStaticData } from "../lib/DataManager";
-
 export async function getStaticProps() {
-  const hospitalsData = getStaticData();
+  const hospitalsData = HospitalJsonToObject();
   return {
     props: {
       hospitalsData,
@@ -46,9 +14,9 @@ export async function getStaticProps() {
   };
 }
 
+
 export default function Home({hospitalsData}) {
-//  const test =  getStaticData()
-//  console.log(test);
+
   return (
     <>
       <Head>
@@ -61,6 +29,42 @@ export default function Home({hospitalsData}) {
     </>
   );
 }
+
+
+// //useSWR allows the use of SWR inside function components
+// import useSWR from 'swr';
+
+// //Write a fetcher function to wrap the native fetch function and return the result of a call to url in json format
+// const fetcher = (url) => fetch(url).then((res) => res.json());
+
+// export default function Index() {
+//   //Set up SWR to run the fetcher function when calling "/api/staticdata"
+//   //There are 3 possible states: (1) loading when data is null (2) ready when the data is returned (3) error when there was an error fetching the data
+//   const { data, error } = useSWR('/api/staticdata', fetcher);
+
+//   //Handle the error state
+//   if (error) return <div>Failed to load</div>;
+//   //Handle the loading state
+//   if (!data) return <div>Loading...</div>;
+//   //Handle the ready state and display the result contained in the data object mapped to the structure of the json file
+//   return (
+//     <div>
+//       <h1>My Framework from file</h1>
+//       <ul>
+//         <li>Name: {data.record.name}</li>
+//         <li>Language: {data.record.language}</li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+
+
+
+
+
+
+
 // export default function Home({ hospitalsData }) {
   
 //   return (
